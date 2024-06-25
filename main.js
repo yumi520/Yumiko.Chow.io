f6a802394b3980545444132beb90ec2bc712b604
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2024-06-25 16:46:57 -0400

    deleted html files from previous iterations

--- main.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.works-link').addEventListener('click', scrollToWorks);
-
-function scrollToWorks() {
-    let worksSection = document.querySelector('.works-section');
-    
-    if (worksSection) {
-        worksSection.scrollIntoView({ behavior: 'smooth' });
-    }
-}
